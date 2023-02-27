0e7a28cd7a483001f61c08949243bd289648540d
Sivasurya Santhanam <sivasurya.in@gmail.com>
2023-02-27 17:20:44 +0100

    image rendering

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def blabla():
-    return "Hello world from Sivasurya"
+def index():
+    image = url_for('static', filename='drjohn.jpg')
+    return render_template('index.html', imag=image)
 
 if __name__ == "__main__":
     app.run(debug=True)
