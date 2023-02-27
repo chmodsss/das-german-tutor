20b7dd09685e4fdf4c8ee57af796b1c9075e5251
Sivasurya Santhanam <sivasurya.in@gmail.com>
2023-02-27 18:29:37 +0100

    chat added

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, url_for
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     image = url_for('static', filename='drjohn.jpg')
     return render_template('index.html', imag=image)
 
+def messageReceived(methods=['GET','POST']):
+    print("Message was received")
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET','POST']):
+    print("Received my event ", json)
+    socketio.emit('my response', json, callback=messageReceived)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
